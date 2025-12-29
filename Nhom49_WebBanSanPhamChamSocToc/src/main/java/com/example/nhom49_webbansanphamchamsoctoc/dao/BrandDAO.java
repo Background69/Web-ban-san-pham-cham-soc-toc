afef546f016705397b24f4c1e948e88f61087cd2
Admin <23130299@st.hcmuaf.edu.vn>
2025-12-29 20:38:40 +0700

    Thêm các lớp DAO cơ bản dựa theo model và tích hợp JDBIConnector, đồng thời triển khai các phương thức CRUD mặc định từ impl qua

--- Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/dao/BrandDAO.java
+++ Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/dao/BrandDAO.java
@@ -1,4 +1,39 @@
 package com.example.nhom49_webbansanphamchamsoctoc.dao;
 
-public class BrandDAO {
+import com.example.nhom49_webbansanphamchamsoctoc.database.JDBIConnector;
+import com.example.nhom49_webbansanphamchamsoctoc.model.Brand;
+import org.jdbi.v3.core.Jdbi;
+
+import java.util.List;
+
+public class BrandDAO implements IDAO<Brand>{
+    private final Jdbi jdbi;
+
+    public BrandDAO() {
+        this.jdbi = JDBIConnector.getInstance();
+    }
+    @Override
+    public Brand findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Brand> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public int insert(Brand entity) {
+        return 0;
+    }
+
+    @Override
+    public boolean update(Brand entity) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return false;
+    }
 }
