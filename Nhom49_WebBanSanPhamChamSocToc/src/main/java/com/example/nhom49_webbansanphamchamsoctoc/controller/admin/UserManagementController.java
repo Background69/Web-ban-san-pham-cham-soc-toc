e4e17023efcdf09e563371149f66abac4bc322db
ndtp41206astrology <23130253@st.hcmuaf.edu.vn>
2026-01-10 16:47:39 +0700

    Chỉnh sửa UserManagementController CategoryManagementController và ProductManagementController

--- Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/controller/admin/UserManagementController.java
+++ Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/controller/admin/UserManagementController.java
@@ -1,18 +1,36 @@
 package com.example.nhom49_webbansanphamchamsoctoc.controller.admin;
 
+import com.example.nhom49_webbansanphamchamsoctoc.dao.UserDAO;
+import com.example.nhom49_webbansanphamchamsoctoc.model.User;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "UserManagementController", value = "/UserManagementController")
 public class UserManagementController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        HttpSession session = request.getSession(false);
+        UserDAO userDAO = new UserDAO();
+        //Nếu chưa đăng nhập chuyển sang trang Login
+        if (session == null || session.getAttribute("currentUser") == null) {
+            response.sendRedirect(request.getContextPath() + "/login");
+            return;
+        }
+        User currentUser = (User) session.getAttribute("currentUser");
+        //Check có phải role Admin hay không
+        if (!"Admin".equalsIgnoreCase(currentUser.getRole())) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Không có quyeefn truy cập");
+            return;
+        }
+        List<User> users = userDAO.findAll();
+        request.setAttribute("user", users);
+        request.getRequestDispatcher("view/admin/users")
+                .forward(request, response);
     }
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
