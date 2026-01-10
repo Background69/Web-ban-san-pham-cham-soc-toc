d1c60860cdee802c4b72604534016f96caf5a0ee
ndtp41206astrology <23130253@st.hcmuaf.edu.vn>
2026-01-10 16:46:44 +0700

    Chỉnh sửa AdminDashBoardController

--- Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/controller/admin/AdminDashBoardController.java
+++ Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/controller/admin/AdminDashBoardController.java
@@ -1,35 +1,41 @@
 package com.example.nhom49_webbansanphamchamsoctoc.controller.admin;
 
 import com.example.nhom49_webbansanphamchamsoctoc.dao.IDAO;
+import com.example.nhom49_webbansanphamchamsoctoc.dao.ProductDAO;
 import com.example.nhom49_webbansanphamchamsoctoc.dao.UserDAO;
+import com.example.nhom49_webbansanphamchamsoctoc.model.User;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
-@WebServlet("/admin")
-public class AdminDashBoard extends HttpServlet {
+@WebServlet("/admin/dashboard")
+public class AdminDashBoardController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            HttpSession session = request.getSession(false);
-            if (session==null||session.getAttribute("currentUser")==null)
-            {
-                response.sendRedirect(request.getContextPath()+"/login");
-                return;
-            }
-            String role = (String) session.getAttribute("role");
-            if (!"admin".equals(role)){
-                response.sendError(HttpServletResponse.SC_FORBIDDEN, "Bạn không có quyền truy cập");
-                return;
-            }
-            request.getRequestDispatcher("/admin/dashboard.jsp").forward(request, response);
-            UserDAO userDAO= new UserDAOImpl();
-            int userCount = userDAO.countUsers();
-            request.setAttribute("userCount", userCount);
-
-
+         HttpSession session = request.getSession(false);
+         UserDAO userDAO = new UserDAO();
+         ProductDAO productDAO = new ProductDAO();
+         //Nếu chưa đăng nhập chuyển sang trang Login
+        if (session==null|| session.getAttribute("currentUser")==null){
+            response.sendRedirect(request.getContextPath()+"/login");
+            return;
+        }
+        User currentUser = (User) session.getAttribute("currentUser");
+        //Check có phải role Admin hay không
+        if (!"Admin".equalsIgnoreCase(currentUser.getRole())){
+            response.sendError(HttpServletResponse.SC_FORBIDDEN,"Không có quyeefn truy cập");
+            return;
+        }
+        int userCount = userDAO.countUsers();
+        int productCount = productDAO.countAll();
+        request.setAttribute("userCount",userCount);
+        request.setAttribute("productCount",productCount);
+        request.getRequestDispatcher("views/admin/dashboard.jsp")
+                .forward(request,response);
     }
+
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
