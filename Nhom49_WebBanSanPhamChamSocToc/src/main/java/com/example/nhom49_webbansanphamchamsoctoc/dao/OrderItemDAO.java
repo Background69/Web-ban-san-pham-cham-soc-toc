b36ac2e341fb037ea1efeed71b1ccb4183a41adf
ndtp41206astrology <23130253@st.hcmuaf.edu.vn>
2026-01-10 16:56:36 +0700

    Update dashboard.jsp

--- Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/dao/OrderItemDAO.java
+++ Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/dao/OrderItemDAO.java
@@ -6,18 +6,28 @@ import com.example.nhom49_webbansanphamchamsoctoc.model.OrderItem;
 import java.util.List;
 
 public class OrderItemDAO implements IDAO<OrderItem> {
-    }
     @Override
     public OrderItem findById(int id) {
+        return null;
     }
 
     @Override
+    public List<OrderItem> findAll() {
+        return List.of();
     }
+
     @Override
+    public int insert(OrderItem entity) {
+        return 0;
     }
 
     @Override
     public boolean update(OrderItem entity) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(int id) {
         return false;
     }
 }
