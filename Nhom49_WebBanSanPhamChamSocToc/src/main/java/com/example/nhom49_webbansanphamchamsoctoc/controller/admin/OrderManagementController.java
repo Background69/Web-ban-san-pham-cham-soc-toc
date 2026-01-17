b68ce8e895c0c8d9c085adfd755d7cd18ce87f92
ndtp41206astrology <23130253@st.hcmuaf.edu.vn>
2026-01-17 22:22:21 +0700

    HairConditionDAO

--- Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/controller/admin/OrderManagementController.java
+++ Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/controller/admin/OrderManagementController.java
@@ -1,5 +1,8 @@
 package com.example.nhom49_webbansanphamchamsoctoc.controller.admin;
 
+import com.example.nhom49_webbansanphamchamsoctoc.dao.ProductDAO;
+import com.example.nhom49_webbansanphamchamsoctoc.model.Product;
+import com.example.nhom49_webbansanphamchamsoctoc.model.User;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -10,9 +13,20 @@ import java.io.IOException;
 public class OrderManagementController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        HttpSession session = request.getSession(false);
+        ProductDAO productDAO = new ProductDAO();
+        //Nếu chưa đăng nhập chuyển sang trang đăng nhập
+        if (session == null || session.getAttribute("currentUser") == null) {
+            response.sendRedirect(request.getContextPath() + "/login");
+            return;
+        }
+        User currentUser = (User) session.getAttribute("currentUser");
+        //Check có phải role Admin hay không
+        if (!"Admin".equalsIgnoreCase(currentUser.getRole())) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Không có quyeefn truy cập");
+            return;
+        }
     }
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
