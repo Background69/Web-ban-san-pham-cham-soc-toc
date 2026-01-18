129a2d28e28d84f4c049a58a950936ee17252491
ndtp41206astrology <23130253@st.hcmuaf.edu.vn>
2026-01-18 23:19:19 +0700

    Hoàn thành HairConditionDAO và PromotionDAO

--- Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/dao/HairConditionDAO.java
+++ Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/dao/HairConditionDAO.java
@@ -4,6 +4,8 @@ import com.example.nhom49_webbansanphamchamsoctoc.database.JDBIConnector;
 import com.example.nhom49_webbansanphamchamsoctoc.model.HairCondition;
 import org.jdbi.v3.core.Jdbi;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class HairConditionDAO implements IDAO<HairCondition> {
@@ -11,21 +13,21 @@ public class HairConditionDAO implements IDAO<HairCondition> {
 
     @Override
     public HairCondition findById(int id) {
-
-        return jdbi.withHandle(handle -> handle.createQuery(
-                        "SELECT condition_id, condition_name, condition_slug" + "FROM hair_conditions WHERE condition_id=:id")
-                .bind("id", id)
-                .mapToBean(HairCondition.class)
-                .findOne()
+                 String sql ="SELECT * FROM hair_conditions WHERE condition_id=:id";
+                 return jdbi.withHandle(handle -> handle.createQuery(sql)
+                 .bind("id", id)
+                .map((rs,ctx)-> mapHairCondition(rs))
+                .findFirst()
                 .orElse(null));
     }
 
     @Override
     public List<HairCondition> findAll() {
+        String sql = "SELECT condition_id, condition_name, condition_slug FROM hair_conditions";
         return jdbi.withHandle(handle ->
-                handle.createQuery(
-                                "SELECT condition_id, condition_name, condition_slug FROM hair_conditions")
-                        .mapToBean(HairCondition.class)
+                handle.createQuery(sql)
+
+                        .map((rs, ctx)->mapHairCondition(rs))
                         .list()
         );
     }
@@ -63,4 +65,16 @@ public class HairConditionDAO implements IDAO<HairCondition> {
         );
         return rows >0;
     }
+    private HairCondition mapHairCondition(ResultSet rs) {
+        try {
+            HairCondition h = new HairCondition();
+            h.setConditionId(rs.getInt("condition_id"));
+            h.setConditionName(rs.getString("condition_name"));
+            h.setConditionSlug(rs.getString("condition_slug"));
+            return h;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/dao/PromotionDAO.java
+++ Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/dao/PromotionDAO.java
@@ -1,32 +1,104 @@
 package com.example.nhom49_webbansanphamchamsoctoc.dao;
 
+import com.example.nhom49_webbansanphamchamsoctoc.database.JDBIConnector;
 import com.example.nhom49_webbansanphamchamsoctoc.model.Promotion;
+import org.jdbi.v3.core.Jdbi;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.List;
 
 public class PromotionDAO implements IDAO<Promotion> {
+    private final Jdbi jdbi = JDBIConnector.getInstance();
     @Override
     public Promotion findById(int id) {
-        return null;
+        String sql = "SELECT * FROM promotion WHERE id=: id";
+        return jdbi.withHandle(handle ->
+                 handle.createQuery(sql)
+                         .bind("id", id)
+                         .map((rs, ctx)->mapPromotion(rs))
+                         .findFirst()
+                         .orElse(null)
+        );
     }
 
     @Override
     public List<Promotion> findAll() {
-        return List.of();
+        String sql = "SELECT * FROM promotions";
+        return jdbi.withHandle(handle ->
+                handle.createQuery(sql)
+                        .map((rs, ctx) -> mapPromotion(rs))
+                        .list()
+        );
     }
 
     @Override
     public int insert(Promotion entity) {
-        return 0;
+        String sql = """
+                INSERT INTO promotions
+                (discount_percent, start_date, end_date, is_active)
+                VALUES (:discount, :start, :end, :active)
+                """;
+        return jdbi.withHandle(handle ->
+                handle.createUpdate(sql)
+                        .bind("discount", entity.getDiscountPercent())
+                        .bind("start", entity.getStartDate())
+                        .bind("end", entity.getEndDate())
+                        .bind("active", entity.isActive())
+                        .execute()
+        );
     }
 
     @Override
     public boolean update(Promotion entity) {
-        return false;
+        String sql = """
+                UPDATE promotions
+                SET 
+                    discount_percent = :discount,
+                    start_date = :start,
+                    end_date = :end,
+                    is_active = :active
+                WHERE promotion_id = :id
+                """;
+        int rows = jdbi.withHandle(handle ->
+                handle.createUpdate(sql)
+
+                        .bind("discount", entity.getDiscountPercent())
+                        .bind("start", entity.getStartDate())
+                        .bind("end", entity.getEndDate())
+                        .bind("active", entity.isActive())
+                        .bind("id", entity.getPromotionId())
+                        .execute()
+        );
+        return rows > 0;
     }
 
     @Override
     public boolean delete(int id) {
-        return false;
+        String sql = "DELETE FROM promotions WHERE promotion_id = :id";
+        int rows = jdbi.withHandle(handle ->
+                handle.createUpdate(sql)
+                        .bind("id", id)
+                        .execute()
+        );
+        return rows > 0;
     }
+    private Promotion mapPromotion(ResultSet rs) throws SQLException {
+        Promotion p = new Promotion();
+        p.setPromotionId(rs.getInt("promotion_id"));
+        p.setDiscountPercent(rs.getInt("discount_percent"));
+
+        Timestamp start = rs.getTimestamp("start_date");
+        Timestamp end = rs.getTimestamp("end_date");
+
+        p.setStartDate(start != null ? start.toLocalDateTime() : null);
+        p.setEndDate(end != null ? end.toLocalDateTime() : null);
+
+        p.setActive(rs.getBoolean("is_active"));
+        p.setCreatedAt(Timestamp.valueOf(rs.getTimestamp("created_at").toLocalDateTime()));
+
+        return p;
+    }
+
 }
