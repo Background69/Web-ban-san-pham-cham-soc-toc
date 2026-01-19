7c76c6cb6a6ca8d03c821bd60f355f8e7c0b32cf
Admin <23130299@st.hcmuaf.edu.vn>
2026-01-19 12:50:32 +0700

    OrderHistoryController để hiển thị lịch sử đơn hàng của người dùng cùng với OrderService và history.jsp

--- Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/controller/user/OrderHistoryController.java
+++ Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/controller/user/OrderHistoryController.java
@@ -1,5 +1,7 @@
 package com.example.nhom49_webbansanphamchamsoctoc.controller.user;
 
+import com.example.nhom49_webbansanphamchamsoctoc.model.User;
+import com.example.nhom49_webbansanphamchamsoctoc.services.OrderService;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -8,13 +10,22 @@ import java.io.IOException;
 
 @WebServlet(name = "OrderHistoryController", value = "/OrderHistoryController")
 public class OrderHistoryController extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private OrderService orderService;
 
+    @Override
+    public void init() throws ServletException {
+        orderService = new OrderService();
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        User user = (User) session.getAttribute("user");
 
+        request.setAttribute("orders", orderService.getOrdersByUser(user.getUserId()));
+        request.setAttribute("orderService", orderService);
+
+        request.getRequestDispatcher("/user/order/history.jsp").forward(request, response);
     }
 }
