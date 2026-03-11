979f8cef86ee6b9dee1d28b0d43f3d78cd434a0c
Admin <23130299@st.hcmuaf.edu.vn>
2026-03-11 21:42:24 +0700

    xóa setCharacterEncoding không cần thiết, refactor lại import, thêm kiểm tra input an toàn với ValidationUtil trong các controller (xin đừng xóa cái sendError trong Post)

--- Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/controller/admin/ProductManagementController.java
+++ Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/controller/admin/ProductManagementController.java
@@ -89,7 +89,6 @@ public class ProductManagementController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        request.setCharacterEncoding("UTF-8");
         String action = request.getParameter("action");
 
         if ("create".equals(action)) {
