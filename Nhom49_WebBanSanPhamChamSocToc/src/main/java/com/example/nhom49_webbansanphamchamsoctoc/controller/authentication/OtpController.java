11fa5bca9678bcffb01c530bf0d8edc172ee5356
Azure <ducphamtan2098@gmail.com>
2026-03-20 11:49:38 +0700

    feat: add OTP verification page with styling, countdown timer, and input handling (unfinished: need review, constraint, change redirect flow, when to send mail, when to resend mail, when to restart timer, what happen when click submit or resend otp)

--- Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/controller/authentication/OtpController.java
+++ Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/controller/authentication/OtpController.java
@@ -1,4 +1,25 @@
 package com.example.nhom49_webbansanphamchamsoctoc.controller.authentication;
 
-public class OtpController {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "OtpController", value = "/auth/verify-otp")
+public class OtpController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/authentication/otp-verification.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
 }
