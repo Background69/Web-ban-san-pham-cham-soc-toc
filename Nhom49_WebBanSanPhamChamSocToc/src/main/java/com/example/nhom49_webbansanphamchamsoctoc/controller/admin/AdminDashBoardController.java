27d08564f19eed437c7ce9e67a544b02c7fd73b3
ndtp41206astrology <23130253@st.hcmuaf.edu.vn>
2026-03-22 23:41:22 +0700

    Thêm vào phần filter doanh thu trong Dashboard (chỉnh sửa trong AdminDashBoardController và OrderDAO)

--- Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/controller/admin/AdminDashBoardController.java
+++ Nhom49_WebBanSanPhamChamSocToc/src/main/java/com/example/nhom49_webbansanphamchamsoctoc/controller/admin/AdminDashBoardController.java
@@ -12,6 +12,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Servlet hiển thị Admin Dashboard
@@ -45,7 +46,27 @@ public class AdminDashBoardController extends HttpServlet {
 
             // Đơn hàng gần nhất (CHỈ SET 1 LẦN)
             request.setAttribute("recentOrders", orderDAO.findRecentOrder(5));
-
+        String type = request.getParameter("type");
+        if (type==null) type = "week";
+            List<Integer> revenueData;
+            String revenueLabels;
+            switch (type){
+                case "month":
+                    revenueData = orderDAO.getRevenueByMonth();
+                    revenueLabels = "[\"T1\",\"T2\",\"T3\",\"T4\",\"T5\",\"T6\",\"T7\",\"T8\",\"T9\",\"T10\",\"T11\",\"T12\"]";
+                    break;
+                case "year":
+                    revenueData = orderDAO.getRevenueByYear();
+                    revenueLabels = "[\"2022\",\"2023\",\"2024\",\"2025\",\"2026\"]";
+                    break;
+                default:
+                    revenueData = orderDAO.getRevenueByWeek();
+                    revenueLabels = "[\"Thứ 2\",\"Thứ 3\",\"Thứ 4\",\"Thứ 5\",\"Thứ 6\",\"Thứ 7\",\"Chủ Nhật\"]";
+                    break;
+            }
+           request.setAttribute("type",type);
+            request.setAttribute("revenueLabels",revenueLabels);
+            request.setAttribute("revenueData",revenueData.toString());
         } catch (Exception e) {
             e.printStackTrace();
             request.setAttribute("dashboardError", "Không tải được dữ liệu");
